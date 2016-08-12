519bd5b2d9d7215afe4e91be65465b684a013cda
Nicolas Herbaut <nherbaut@labri.fr>
2016-08-12 15:57:22 +0200

    sarimad.pdf

--- ystats/slagen.py
+++ ystats/slagen.py
@@ -9,11 +9,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib import animation
 
 matplotlib.style.use('ggplot')
+win = 7
 
-fig = plt.figure()
+
 
 # read data
 df = pd.read_csv("test2.csv", names=["time", "values"])
@@ -26,13 +26,15 @@ ts = ts[-50:]
 # tsr=ts.resample("1H").max().ffill()
 tsr = ts
 
-# create the figure for monoplot & plot bar on the same axis
 
-# first plot the line
-ax = plt.axes(xlim=[tsr.index[0], tsr.index[-1]], ylim=[min(tsr) * 0.75, max(tsr) * 1.25])
-ax.xaxis_date()
-line2d = ax.plot(tsr.index, tsr, zorder=1)
-bar = ax.bar([], [])
+def print_forecast_and_discretization(tsr, tse):
+    fig = plt.figure()
+    # first plot the line
+    ax = fig.add_axes(xlim=[tsr.index[0], tsr.index[-1]], ylim=[min(tsr) * 0.75, max(tsr) * 1.25])
+    ax.xaxis_date()
+    line2d = ax.plot(tsr.index, tsr, zorder=1)
+    # create the figure for monoplot & plot bar on the same axis
+    bar = ax.bar(tse.index, tse, width=1 / 24.0, zorder=1)
 
 
 def get_tse(tsr, win):
@@ -42,15 +44,12 @@ def get_tse(tsr, win):
     offset = pd.tseries.offsets.Hour(win)
     tsrr = tsr.resample("1H").bfill()
     tse = pd.Series(index=tsrr.index)
-
     start = tsr.index[0]
     end = tsr.index[-1]
     for i in range(0, int((end - start) / offset)):
         range_max = np.max(tsrr[(start + i * offset):(start + (i + 1) * offset)].values)
         tse[(start + i * offset):(start + (i + 1) * offset)] = range_max
-
     tse[(start + (i + 1) * offset):end] = np.max(tsrr[(start + (i + 1) * offset):end].values)
-
     # print(tse)
     return tse
 
@@ -60,16 +59,16 @@ def get_plot(win):
     tse = get_tse(tsr, win + 1)
     # then display the bar on the same axis
     # bar, = ax1.plot([], [], width=0.041, axes=line2d[0].axes)
-    bar = ax.bar(tse.index, tse, width=1/24.0, zorder=1)
+    bar = ax.bar(tse.index, tse, width=1 / 24.0, zorder=1)
     return bar
 
 
-def init():
-    return line2d
-
-
-get_plot(2)
-
-#anim = animation.FuncAnimation(fig, get_plot, init_func=init, frames=20, interval=300, blit=True)
-#anim.save('basic_animation.mp4',)
-plt.show()
+# get_plot(7)
+# anim = animation.FuncAnimation(fig, get_plot, init_func=init, frames=20, interval=300, blit=True)
+# anim.save('basic_animation.mp4',)
+tse = get_tse(tsr, win + 1)
+print_forecast_and_discretization(tsr, tse)
+plt.show(block=False)
+while len(tse) > 5:
+    raw_input("Press Enter to continue...")
+    plt.show(block=False)
